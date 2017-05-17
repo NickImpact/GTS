029162a37d905175e714d91cef4d1b7ba5a14d7b
Nick <nick.degruccio@gmail.com>
2017-05-17 00:20:00 -0700

    Increment build from Snapshot --> Stable

--- src/main/java/com/nickimpact/GTS/GTSInfo.java
+++ src/main/java/com/nickimpact/GTS/GTSInfo.java
@@ -17,7 +17,7 @@ public class GTSInfo {
     public static final String ID = "gts";
 
     public static final String NAME = "GTS";
-    public static final String VERSION = "2.2.0-SNAPSHOT";
+    public static final String VERSION = "2.2.0";
     public static final String DESCRIPTION = "A Sponge Representation of the Global Trading Station";
 
     public static final Text PREFIX = Text.of(TextColors.YELLOW, "GTS ", TextColors.GRAY, "\u00bb ", TextColors.DARK_AQUA);
