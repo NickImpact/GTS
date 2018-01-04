f0667a2044cfd57a39f89ce62b70b137eadc4e7b
Nick <nick.degruccio@gmail.com>
2018-01-03 16:26:57 -0800

    Use correct index for primary keys, fixes #4

--- src/main/resources/schema/mysql.sql
+++ src/main/resources/schema/mysql.sql
@@ -4,26 +4,26 @@ CREATE TABLE `{prefix}listings` (
   `id`        INT         NOT NULL,
   `uuid`      VARCHAR(36) NOT NULL,
   `listing`   MEDIUMTEXT  NOT NULL,
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8;
 
 CREATE TABLE `{prefix}logs` (
   `id`        INT         NOT NULL,
   `uuid`      VARCHAR(36) NOT NULL,
   `log`       MEDIUMTEXT  NOT NULL,
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8;
 
 CREATE TABLE `{prefix}held_entries` (
   `id`        INT         NOT NULL,
   `holder`    MEDIUMTEXT  NOT NULL,
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8;
 
 CREATE TABLE `{prefix}held_prices` (
   `id`        INT         NOT NULL,
   `holder`    MEDIUMTEXT  NOT NULL,
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 ) DEFAULT CHARSET = utf8;
 
 CREATE TABLE `{prefix}ignorers` (
