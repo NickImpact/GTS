d997dec1e22286a750d07f89e43da5f672e9f120
Nick DeGruccio <nick.degruccio@gmail.com>
2021-03-31 09:03:10 -0700

    Snapshot

---
+++ api/src/main/java/net/impactdev/gts/api/commands/GTSCommandExecutor.java
@@ -0,0 +1,27 @@
+package net.impactdev.gts.api.commands;
+
+import com.google.common.collect.Lists;
+import net.impactdev.gts.api.commands.annotations.Alias;
+import net.impactdev.gts.api.commands.annotations.Permission;
+
+import java.util.List;
+
+public interface GTSCommandExecutor<E, S> {
+
+    void register();
+
+    default List<String> getAliases() {
+        return Lists.newArrayList(this.getClass().getAnnotation(Alias.class).value());
+    }
+
+    E[] getArguments();
+
+    GTSCommandExecutor<E, S>[] getSubcommands();
+
+    S build();
+
+    default boolean hasNeededAnnotations() {
+        return this.getClass().isAnnotationPresent(Alias.class) && this.getClass().isAnnotationPresent(Permission.class);
+    }
+
+}
