51735ab91e4365db8cf14a04d1e6aa3dbef7fef2
Nick DeGruccio <nick.degruccio@gmail.com>
2021-05-26 09:03:27 -0700

    Fix imports not existing from recent PRs

--- plugin/src/main/java/net/impactdev/gts/listeners/JoinListener.java
+++ plugin/src/main/java/net/impactdev/gts/listeners/JoinListener.java
@@ -9,13 +9,17 @@ import net.impactdev.gts.util.OreVersionChecker;
 import net.impactdev.impactor.api.Impactor;
 import net.impactdev.impactor.api.plugin.PluginMetadata;
 import net.impactdev.impactor.api.services.text.MessageService;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.effect.sound.SoundTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.text.Text;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class JoinListener {
 
@@ -23,16 +27,16 @@ public class JoinListener {
     public void onAdminJoin(ClientConnectionEvent.Join event, @First Player player) {
         final MessageService<Text> parser = Impactor.getInstance().getRegistry().get(MessageService.class);
 
-        if(player.hasPermission(GTSPermissions.ADMIN_BASE)) {
+        if (player.hasPermission(GTSPermissions.ADMIN_BASE)) {
             PluginMetadata meta = GTSPlugin.getInstance().getMetadata();
             final Version current = new Version(meta.getVersion());
             CompletableFuture<Version> ore = OreVersionChecker.query();
             ore.thenAccept(response -> {
-                if(current.compareTo(response) < 0) {
+                if (current.compareTo(response) < 0) {
                     String before = Utilities.readMessageConfigOption(MsgConfigKeys.UPDATE_AVAILABLE);
                     String after = before.replace("{{new_version}}", response.toString());
                     player.sendMessage(parser.parse(after));
-                } else if(current.isSnapshot()) {
+                } else if (current.isSnapshot()) {
                     player.sendMessage(parser.parse(Utilities.readMessageConfigOption(MsgConfigKeys.UPDATE_SNAPSHOT)));
                 } else {
                     player.sendMessage(parser.parse(Utilities.readMessageConfigOption(MsgConfigKeys.UPDATE_LATEST)));
@@ -41,20 +45,20 @@ public class JoinListener {
         }
 
     }
-    
+
     @Listener
     public void onPlayerLogin(ClientConnectionEvent.Login event) {
         Sponge.getScheduler().createTaskBuilder().execute(() -> {
-        final MessageService<Text> parser = Impactor.getInstance().getRegistry().get(MessageService.class);
+            final MessageService<Text> parser = Impactor.getInstance().getRegistry().get(MessageService.class);
             final Optional<Player> player = Sponge.getServer().getPlayer(event.getTargetUser().getUniqueId());
-            GTSPlugin.getInstance().getStorage().getStash( player.get().getUniqueId() ).thenAccept(
-                stash -> {
-                    if (!stash.isEmpty()) {
-                        player.get().sendMessage( parser.parse( Utilities.readMessageConfigOption( MsgConfigKeys.STASH_COLLECT_JOIN_MESSAGE ) ) );
-                        player.get().playSound( SoundTypes.BLOCK_CHEST_OPEN, player.get().getPosition(), 1 );
-                    }
-                });
-        }).delay( 4, TimeUnit.SECONDS).submit(GTSPlugin.getInstance().getBootstrap());
+            GTSPlugin.getInstance().getStorage().getStash(player.get().getUniqueId()).thenAccept(
+                    stash -> {
+                        if (!stash.isEmpty()) {
+                            player.get().sendMessage(parser.parse(Utilities.readMessageConfigOption(MsgConfigKeys.STASH_COLLECT_JOIN_MESSAGE)));
+                            player.get().playSound(SoundTypes.BLOCK_CHEST_OPEN, player.get().getPosition(), 1);
+                        }
+                    });
+        }).delay(4, TimeUnit.SECONDS).submit(GTSPlugin.getInstance().getBootstrap());
     }
 
 }
